2bfcb96ef2a3cbdbfa8d5b0ad971a3a4a66248e3
FarzamN <89484118+FarzamN@users.noreply.github.com>
2024-06-18 17:26:35 +0500

    replace method

---
+++ main.py
@@ -0,0 +1,28 @@
+# print("Hello world")
+# import pyjokes
+
+# print(joke)
+
+# import pyjokes
+# joke = pyjokes.get_joke(language="en", category="neutral")
+# # joke_Two = pyjokes.get_joke(language="en", category="chucknorris")
+# print(joke)
+# print(5005+ 500)
+
+# InputNumberOne = int(input("Enter number 1: "))
+# InputNumberTwo = int(input("Enter number 2: "))
+
+# print(f"checking if a is greater then b, {InputNumberOne > InputNumberTwo}")
+
+# replace function 
+
+# replaceLetter = '''
+# Dear Name|>, You are selected! Date|> '''
+# print(replaceLetter.replace("Name|>", "Farzam").replace("Date|>", "24 Sep 2020"))
+
+# sentence = "hello world   this is python practice section"
+# print(sentence.replace("   "," "))
+
+# sentence = "hello world\n \tThis is python practice section"
+# print(sentence)
+
