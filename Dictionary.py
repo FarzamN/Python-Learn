e1da540e37d8645a6625166a1993af874a565b3e
FarzamN <89484118+FarzamN@users.noreply.github.com>
2024-06-21 00:49:24 +0500

    Dictionary start

---
+++ Dictionary.py
@@ -0,0 +1,18 @@
+# dictionary is like json for example
+students = {
+    "Farzam": 100,
+    "Fahad": 500,
+    "Faaz": 10,
+}
+
+# now to print
+print(students["Farzam"])
+
+# Properties
+
+"""
+ • It is unordered (order doesnot metter)
+ • It is mutable (you can add edit perticular item)
+ • It is indexed (will not check every item)
+ • Can't contain same key
+"""
