354308d7120c6c4e6fc66ec095e0a518ab848e83
FarzamN <89484118+FarzamN@users.noreply.github.com>
2024-06-24 20:00:43 +0500

    set Practice and loops

--- Practice.py
+++ Practice.py
@@ -1,3 +1,4 @@
+import numbers
 """
 # 1) Write a program to store seven fruits in a list entered by user.
 
@@ -42,7 +43,6 @@ countTup = (7,0,8,0,0,9)
 counted= countTup.count(0)
 print(counted)
 
-"""
 
 # Dictionary and Sets Practice
 # 1) Write a program to create a dictionary of Hindi words with values as their English translation. Provide the user an option to look up the meaning of a word.
@@ -59,4 +59,37 @@ print(words.get(word, "Word not found in Words"))
 
 # 2) Write a program to input eight numbers from user and display the all unique numbers 
 
-# for i in range(8):
+sets = set()
+for i in range(3):
+    n = input(f"Enter 0{i+1} number: ")
+    sets.add(n)
+print(f"Here is the final value {sets}")
+
+# 3) Can we have a set with 18(int) and "18"(str) as a value in it?
+ 
+sets = {18, "18"}
+print(sets) # it will print both values as it is set
+
+# 4) What will be the length of the following set Sets
+
+Sets = {18, "18",18.00}
+print(len(Sets)) # flooting number will be = 18
+
+# 5) create an empty dictionary. Allow 4 friends to enter their favourite language and include the names of your friends in the dictionary. Print the dictionary
+
+favLang = {}
+
+for i in range(2):
+    name = input(f"Enter your Name {i+1}: ")
+    lang = input(f"Enter your Favourite Language {i+1}: ")
+    # favLang[name] = lang
+    favLang.update({name:lang})
+
+    print(favLang)
+
+# 6) Change value of array
+
+s = {1,2,3,4,"Farzam", [1,2]}
+print(type(s))
+
+"""
---
+++ _loops.py
@@ -0,0 +1,7 @@
+for i in range(1, 11):
+    print(f"for ka data {i}")
+
+    i = 1
+    while i >= 10:
+        print(f"while ka data {i}")
+        i += 1
