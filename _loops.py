12b010d1422e254b6ac60d80edcefa1ee6f7b849
FarzamN <89484118+FarzamN@users.noreply.github.com>
2024-06-26 14:45:24 +0500

    Ioops Learning

--- _loops.py
+++ _loops.py
@@ -1,7 +1,36 @@
+'''
 for i in range(1, 11):
     print(f"for ka data {i}")
 
-    i = 1
-    while i >= 10:
-        print(f"while ka data {i}")
-        i += 1
+i = 1
+while i >= 10:
+    print(f"while ka data {i}")
+    i += 1
+
+listItem = ["Farzam", "Nawal", "Faaz", "Nuflah", "Nasheeta", "Nareeman"]
+listItem = [1,34]
+listIndex= 0
+while listIndex < len(listItem):
+    print(f"while ka data {listItem[listIndex]}")
+    listIndex += 1
+
+for i in range (len(listItem)):
+    print(f"for ka data {listItem[i]}")
+for i in (listItem):
+    print(f"for ka data {i}")
+
+for item in "hello":
+    print(item)
+else :
+    print("Done")
+
+for i in range(11):
+    if i == 5:
+       continue
+    print(f"for ka data {i}")
+'''
+
+for i in range(11):
+    if i == 5:
+       break
+    print(f"for ka data {i}")
