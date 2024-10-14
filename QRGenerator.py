67714ce43cdcf86cba5be315db0678878e0dcecc
FarzamN <89484118+FarzamN@users.noreply.github.com>
2024-10-14 17:16:44 +0500

    qr code generator done
    It was a ducking ez project but it was fun

---
+++ QRGenerator.py
@@ -0,0 +1,12 @@
+"""
+get website url
+"""
+
+import qrcode
+
+url = input("Enter the website URL: ")
+img = qrcode.make(url)
+type(img)  # qrcode.image.pil.PilImage
+fileName = input("Enter the file name: ")
+
+img.save(fileName + ".png" or "qrcode.png")
