9fc75630391cd12205de5dee6392471ffbd0f60c
FarzamN <89484118+FarzamN@users.noreply.github.com>
2024-11-14 00:09:35 +0500

    Nupmy Statted

---
+++ Numpy/one.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+# rangeArr = np.arange(11, 21).reshape((2, 5))
+# print(rangeArr)
+
+"""
+Nupmy arttributes
+1) ndim
+2) shape
+3) size
+4) dtype
+5) itemsize
+"""
+
+identity = np.eye(10)  # 3x3 identity matrix
+
+print(f"{identity}")
